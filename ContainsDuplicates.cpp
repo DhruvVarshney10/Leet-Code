edcb5a23b0e671f3c6f03f34ed501442f8300f9f
DhruvVarshney10 <67312337+DhruvVarshney10@users.noreply.github.com>
2022-01-23 16:58:44 -0800

    Contains Duplicates Array

---
+++ ContainsDuplicates.cpp
@@ -0,0 +1,40 @@
+
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+*/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                cout << nums[i];
+                return true;
+            }
+        }
+        return false;
+
+    }
+};
+
+/*
+Runtime: 184 ms, faster than 11.68% of C++ online submissions for Contains Duplicate.
+Memory Usage: 46.6 MB, less than 57.08% of C++ online submissions for Contains Duplicate.
+*/
